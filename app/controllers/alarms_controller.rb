0525e710522afd947ab392fa697f71fd17165594
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-02 02:36:09 +0900

    Add method to stop alarm in alarms controller

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -2,9 +2,16 @@
 class AlarmsController < ApplicationController
 
   def ring
+    stop if params[:time] == '0'
     wake_up_time = params[:time].insert(-3, ':')
     `/home/rasp-yyh/smart-home/Alarm/alarm.sh #{wake_up_time}`
     head :ok
+  end
+
+  private
+  
+  def stop
+    `pidof /usr/bin/mpg321 | xargs kill -9`
   end
   
 end
