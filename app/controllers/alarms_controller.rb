b441ff5fe41de29946cb2f726be2c66e2e2e53a5
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-02 11:19:12 +0900

    Fix alarms controller

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -6,15 +6,16 @@ class AlarmsController < ApplicationController
       stop
       json_message = "stop alarm"
     else
-      today = Time.current
+      today = Time.current      
+      if params[:time].to_i >= today.strftime("%H%M").to_i
+        wake_up_date = today.strftime("%Y/%m/%d - ")
+      else
+        wake_up_date = today.tomorrow.strftime("%Y/%m/%d - ")
+      end
       wake_up_time = params[:time].insert(-3, ':')
       `/home/rasp-yyh/smart-home/Alarm/alarm.sh #{wake_up_time}`
-      if params[:time].to_i >= today.strftime("%H%M").to_i
-        wake_up_date = today.strftime("%Y/%m/%d - ") + wake_up_time
-      else
-        wake_up_date = today.tomorrow.strftime("%Y/%m/%d - ") + wake_up_time
-      end
-      json_message = "set alarm at #{wake_up_date}"      
+       wake_up_date << wake_up_time
+      json_message = "set alarm at #{wake_up_date}"
     end
     json_response({message: json_message})
   end
