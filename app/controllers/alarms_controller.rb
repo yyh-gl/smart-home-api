acab1ecc676378b3d19718d877ec84dc3e9f374e
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-02 10:34:59 +0900

    Add json_respons

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -4,11 +4,13 @@ class AlarmsController < ApplicationController
   def ring
     if params[:time] == '0'
       stop
+      json_response("stop alarm")
     else
       wake_up_time = params[:time].insert(-3, ':')
       `/home/rasp-yyh/smart-home/Alarm/alarm.sh #{wake_up_time}`
+      json_response("set alarm at #{wake_up_time}")
     end
-    head :ok
+    # head :ok    
   end
 
   private
