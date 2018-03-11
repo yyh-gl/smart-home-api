a7b2edadd13344effe172edca21e3ebbb2caf896
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-11 23:38:57 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/weathers', to: 'weathers#speak'
   get '/alarms', to: 'alarms#index'
   post '/alarms/:time', to: 'alarms#ring'
+  delete '/alarms/:time', to: 'alarms#delete'
 end
