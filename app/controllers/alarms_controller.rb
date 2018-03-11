c60ec73d3439a1d74ddcf8fbed3984a650eff1c3
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-12 00:20:16 +0900

    Fix alarms#delete

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -14,22 +14,22 @@ class AlarmsController < ApplicationController
       stop
       json_message = "stop alarm"
     else
-      wake_up_time = params[:time].insert(-3, ':')
-      `/home/rasp-yyh/smart-home/Alarm/alarm.sh #{wake_up_time}`
       wake_up_date = get_wake_up_date(params[:time])
       save_reservation_date(wake_up_date)
       json_message = "set alarm at #{wake_up_date}"
+      wake_up_time = params[:time].insert(-3, ':')
+      `/home/rasp-yyh/smart-home/Alarm/alarm.sh #{wake_up_time}`
     end
     json_response({message: json_message})
   end
 
   # DELETE /alarms/:time
   def delete
-    delete_time = params[:time].insert(-3, ':')
-    `at -l | grep #{delete_time} | awk '{print $1}' | xargs at -d`
     wake_up_date = get_wake_up_date(params[:time])
     delete_reservation_date(wake_up_date)
     json_message = "delete alarm at #{wake_up_date}"
+    delete_time = params[:time].insert(-3, ':')
+    `at -l | grep #{delete_time} | awk '{print $1}' | xargs at -d`
     json_response({message: json_message})
   end
 
@@ -47,10 +47,10 @@ class AlarmsController < ApplicationController
     Alarm.where("reservation_date like '%" + wake_up_date + "%'").delete_all
   end
 
-  def get_wake_up_date(time_str)
-    wake_up_time = time_str.insert(-3, ':')
+  def get_wake_up_date(time)
+    wake_up_time = time.insert(-3, ':')
     today = Time.current
-    if time_str.to_i >= today.strftime("%H%M").to_i
+    if time.to_i >= today.strftime("%H%M").to_i
       return today.strftime("%Y-%m-%d ") << wake_up_time
     end
     return today.tomorrow.strftime("%Y-%m-%d ") << wake_up_time
