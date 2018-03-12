57e8e864cc32dd57038cefe090e3dd08f6020915
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-03-13 01:24:06 +0900

    test

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -52,12 +52,12 @@ class AlarmsController < ApplicationController
   end
 
   def get_reservation_datetime
-    @wake_up_time = params[:time].clone
+    @wake_up_time = params[:time].clone.insert(-3, ':')
     today = Time.current
     if params[:time].to_i >= today.strftime("%H%M").to_i
-      @wake_up_datetime = today.strftime("%Y-%m-%d ") << params[:time].insert(-3, ':')
+      @wake_up_datetime = today.strftime("%Y-%m-%d ") << @wake_up_time
     end
-    @wake_up_datetime = today.tomorrow.strftime("%Y-%m-%d ") << params[:time].insert(-3, ':')
+    @wake_up_datetime = today.tomorrow.strftime("%Y-%m-%d ") << @wake_up_time
   end
 
 end
